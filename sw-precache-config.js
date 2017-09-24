a7380f74c9066b3fd5477f67c3d959364bc926a8
panyaxbo <panya.javamania@gmail.com>
2017-09-24 21:49:02 +0700

    update component

--- sw-precache-config.js
+++ sw-precache-config.js
@@ -1,7 +1,23 @@
+var SWPrecacheWebpackPlugin = require('sw-precache-webpack-plugin')
+
 module.exports = {
     navigateFallback: '/index.html',
+    navigateFallbackWhitelist: [/^(?!\/__)/],
     stripPrefix: 'dist',
     root: 'dist/',
+    plugins: [
+        new SWPrecacheWebpackPlugin({
+            cacheId: 'firestarter',
+            filename: 'service-worker.js',
+            staticFileGlobs: [
+                'dist/index.html',
+                'dist/**.js',
+                'dist/**.css'
+            ],
+            stripPrefix: 'dist/assets/',
+            mergeStaticConfig: true
+        })
+    ],
     staticFileGlobs: [
         'dist/index.html',
         'dist/**.js',
